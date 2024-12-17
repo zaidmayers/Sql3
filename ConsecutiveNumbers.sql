80f243bae4a56588eaf4e3c697038ec1237d2719
Zaid Roshan Mayers <zaidmayers@gmail.com>
2024-12-16 19:50:16 -0800

    Added solutions for ConsecutiveNumbers.sql

---
+++ ConsecutiveNumbers.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT num, 
+           LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+           LEAD(num, 1) OVER (ORDER BY id) AS next_num
+    FROM Logs
+) as sub
+WHERE num = prev_num AND num = next_num;
